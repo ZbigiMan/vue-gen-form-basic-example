0723073236ec732ec2aa21c3a0fb7eaa30b79404
Zbigniew Stepniewski <zbigniew.stepniewski@ironin.pl>
2024-01-18 17:18:28 +0100

    fix vue-gen-form styles import

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import './assets/main.css'
-import 'vue-gen-form/dist/style.css'
 
 const app = createApp(App)
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   mode: 'jit',
   important: true,
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [
+      './index.html', 
+      './src/**/*.{vue,js,ts,jsx,tsx}',
+      'node_modules/vue-gen-form/dist/*.js'
+    ],
   theme: {
     extend: {}
   },
